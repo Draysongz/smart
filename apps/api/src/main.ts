5b7074ea6b7e4c77ea1e7df87306ac130c615a06
Levi <70198955+Draysongz@users.noreply.github.com>
2024-08-21 22:10:13 +0100

    sdo

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Allows all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed methods
+    allowedHeaders: 'Content-Type, Accept', // Specify the allowed headers
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(3000);
 }
 bootstrap();
