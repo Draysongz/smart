54a98c87098d5eb4a5c30c2769a5961d76239b17
Levi <70198955+Draysongz@users.noreply.github.com>
2024-08-21 22:35:58 +0100

    done

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: {
-    origin: false,
+    origin: 'https://smartgreen.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 
   }});
