3d63eb0e52727a4e87dd0b76c63384c711e7f0a3
Levi <70198955+Draysongz@users.noreply.github.com>
2024-08-21 22:31:00 +0100

    dd

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', // Allows all origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed methods
-    allowedHeaders: 'Content-Type, Accept', // Specify the allowed headers
-  });
+  const app = await NestFactory.create(AppModule, {cors: {
+    origin: false,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 
-  app.useGlobalPipes(new ValidationPipe());
-  
+  }});
   await app.listen(3000);
 }
 bootstrap();
