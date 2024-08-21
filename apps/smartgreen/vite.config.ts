7594f152122c71621c51e58b9d3be44107cc0bd0
Levi <70198955+Draysongz@users.noreply.github.com>
2024-08-21 21:15:33 +0100

    done

--- apps/smartgreen/vite.config.ts
+++ apps/smartgreen/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import commonjs from '@rollup/plugin-commonjs'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -17,8 +16,9 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://smart-1-hl3w.onrender.com',
-        changeOrigin: true
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // This line rewrites the URL if necessary
       }
     }
   }
-})
+});
