e18b130c70ed624e36c7993745b25c5f4e06e4e0
Levi <70198955+Draysongz@users.noreply.github.com>
2024-08-24 08:50:23 +0100

    done

--- apps/smartgreen/src/components/ProgressBar.tsx
+++ apps/smartgreen/src/components/ProgressBar.tsx
@@ -1,37 +1,26 @@
-// import { useState, useEffect } from 'react';
 import { Box, Progress } from '@chakra-ui/react';
-import { ChevronRightIcon } from '@chakra-ui/icons'
+import { ChevronRightIcon } from '@chakra-ui/icons';
 import { Users } from 'api-contract';
 
+const levels = ["Spark Initiate", "Current Conductor", "Power Pioneer", "Voltage Vanguard", "Energy Elite", "Quantum Master"];
 
-const levels=["Spark Initiate", "Current Conductor" ,"Power Pioneer", "Voltage Vanguard", "Energy Elite" ,"Quantum Master"] 
+const ProgressBar = ({ userLevel, userData, thresh }: { userLevel: number | undefined, userData: Users, thresh: number[] }) => {
 
-const ProgressBar = ({userLevel, userData, thresh}:{userLevel: number | undefined, userData: Users, thresh: number[]}) => {
-  // const [progress, setProgress] = useState(0);
+  // Guard clauses to ensure the component doesn't break when `userLevel` is undefined
+  if (userLevel === undefined || userLevel < 1 || userLevel > levels.length) {
+    return null;
+  }
 
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     setProgress((oldProgress) => {
-  //       if (oldProgress === 100) {
-  //         clearInterval(timer);
-  //         return oldProgress;
-  //       }
-  //       return Math.min(oldProgress + 10, 100);
-  //     });
-  //   }, 10000); // Update every second
-
-  //   return () => {
-  //     clearInterval(timer);
-  //   };
-  // }, []);
+  // Calculate the progress percentage
+  const progress = (userData.coinsEarned / thresh[userLevel - 1]) * 100;
 
   return (
     <Box width="100%" className='pl-[20px] h-[6vh]'>
-        <span className='text-white flex justify-between w-[115px] text-[12px]'>
-            <p>{levels[userLevel! - 1]} <ChevronRightIcon /> </p>
-            <p>{userLevel}<span className='opacity-50'>/ 6</span></p>
-        </span>
-      <Progress width="110px" value={(userData.coinsEarned / thresh[userLevel! -1]) * 100} height="3px" colorScheme='yellow' className='rounded-full'/>
+      <span className='text-white flex justify-between w-[115px] text-[12px]'>
+        <p>{levels[userLevel - 1]} <ChevronRightIcon /> </p>
+        <p>{userLevel}<span className='opacity-50'>/ 6</span></p>
+      </span>
+      <Progress width="110px" value={progress} height="3px" colorScheme='yellow' className='rounded-full'/>
     </Box>
   );
 };
